c47b0a8842ab286cb2b4dd3ad4f4a8a457bdfb26
Courtney Davis <cedavis12@gmail.com>
2019-08-02 23:30:02 -0400

    Add bootstrap theme, book model, controller and view.

---
+++ Bookly/Bookly/Controllers/BooksController.cs
@@ -0,0 +1,26 @@
+﻿using Bookly.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bookly.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //GET: Books/Random
+        public ActionResult Random()
+        {
+            var movie = new Book() { Name = "Frankenstein" };
+
+            return View(movie);
+        }
+    }
+}
